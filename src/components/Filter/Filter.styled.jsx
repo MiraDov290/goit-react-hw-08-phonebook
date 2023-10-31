e94c08815d59587893c25d05f6c21852312c132c
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-10-31 13:46:19 +0000

    changers

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -1,18 +1,30 @@
 import styled from 'styled-components';
 
 export const Div = styled.div`
-  margin-bottom: 15px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  margin-top: 20px;
+  border: 1px solid darkgray;
+  background-color: white;
+  box-shadow: 0px 2px 10px -3px;
+  border-radius: 5px;
+  width: 420px;
 `;
 
 export const Label = styled.label`
-  color: gray;
-  font-size: 20px;
+  margin: 10px 20px 10px 20px;
+  font-size: 18px;
+    color: gray;
 `;
 
 export const Input = styled.input`
-  width: 400px;
-  padding: 10px;
-  margin: 0 auto;
-  border-radius: 4px;
-  border: 1px solid black;
+  width: 300px;
+  margin-top: 4px;
+  padding: 4px;
+  font-size: 16px;
+  border: 2px solid #000;
+  border-radius: 5px;
+  background-color: ghostwhite;
 `;
+
